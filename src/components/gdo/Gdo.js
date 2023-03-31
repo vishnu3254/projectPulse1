eb87380c86c0bdceba79c875d11400b9f10d8467
vishnu <vishnuvardhan.u@westagilelabs.com>
2023-03-31 15:12:34 +0530

    updates

--- src/components/gdo/Gdo.js
+++ src/components/gdo/Gdo.js
@@ -11,6 +11,7 @@ const Gdo = () => {
     <div className="container">
       <div className="row mt-5 container justify-content-center align-items-center h-auto">
         <div className="col-7 col-sm-5 col-md-4 mx-auto mt-4 h-100">
+          {/* team assinging component */}
           <TeamAssign
             url="http://localhost:4000/gdo-api/gdo/portfolioDashboard"
             teamAssign={teamAssign}
@@ -18,17 +19,20 @@ const Gdo = () => {
           />
         </div>
 
+        {/* raise resource component */}
         <div className="col-7 col-sm-5 col-md-5 mx-auto mt-4 h-100">
           <RaiseResourceRequest url="http://localhost:4000/gdo-api/gdo/portfolioDashboard" />
         </div>
       </div>
 
+      {/* if employee is assigned then show message */}
       {teamAssign && (
         <p className="text-center text-success display-6 fs-4">
           Employee assigned to project Successfully
         </p>
       )}
 
+      {/* get all projects  */}
       <div className="mt-5 mb-5 pb-5">
         <GetProjects
           url="http://localhost:4000/gdo-api/gdo/portfolioDashboard"
